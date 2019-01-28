185d65c917a7e802b08636e837ee276ee68a9e5c
Mert Yumer <45640355+MertYumer@users.noreply.github.com>
2019-01-28 23:16:16 +0200

    Edited P03-CountUppercaseWords and P05-FilterByAge

--- C# Advanced/05-functional-programming/P03-CountUppercaseWords/CountUppercaseWords.cs
+++ C# Advanced/05-functional-programming/P03-CountUppercaseWords/CountUppercaseWords.cs
@@ -7,9 +7,11 @@
     {
         public static void Main()
         {
+            Func<string, bool> isUpper = w => char.IsUpper(w[0]);
+
             var words = Console.ReadLine()
                 .Split(" ", StringSplitOptions.RemoveEmptyEntries)
-                .Where(w => char.IsUpper(w[0]));
+                .Where(isUpper);
 
             Console.WriteLine(string.Join(Environment.NewLine, words));
         }
--- C# Advanced/05-functional-programming/P05-FilterByAge/FilterByAge.cs
+++ C# Advanced/05-functional-programming/P05-FilterByAge/FilterByAge.cs
@@ -8,7 +8,7 @@
         public static void Main()
         {
             int n = int.Parse(Console.ReadLine());
-            var people = new Dictionary<string, int>();
+            var people = new HashSet<Person>();
 
             for (int i = 0; i < n; i++)
             {
@@ -16,7 +16,8 @@
                 string name = input[0];
                 int age = int.Parse(input[1]);
 
-                people.Add(name, age);
+                var person = new Person(name, age);
+                people.Add(person);
             }
 
             string condition = Console.ReadLine();
@@ -24,11 +25,11 @@
             string format = Console.ReadLine();
 
             Func<int, bool> tester = CreateTester(condition, searchedAge);
-            Action<KeyValuePair<string, int>> printer = CreatePrinter(format);
+            Action<Person> printer = CreatePrinter(format);
 
             foreach (var person in people)
             {
-                if (tester(person.Value))
+                if (tester(person.Age))
                 {
                     printer(person);
                 }
@@ -50,22 +51,35 @@
             }
         }
 
-        public static Action<KeyValuePair<string, int>> CreatePrinter(string format)
+        public static Action<Person> CreatePrinter(string format)
         {
             switch (format)
             {
                 case "name":
-                    return person => Console.WriteLine($"{person.Key}");
+                    return person => Console.WriteLine($"{person.Name}");
 
                 case "age":
-                    return person => Console.WriteLine($"{person.Value}");
+                    return person => Console.WriteLine($"{person.Age}");
 
                 case "name age":
-                    return person => Console.WriteLine($"{person.Key} - {person.Value}");
+                    return person => Console.WriteLine($"{person.Name} - {person.Age}"); 
 
                 default:
                     return null;
             }
+        }
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
         }
     }
 }
