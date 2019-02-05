aff91420ea9761931b87ab6f8627b2ab65540015
Mert Yumer <45640355+MertYumer@users.noreply.github.com>
2019-02-05 21:44:57 +0200

    Added new lab

---
+++ C# Advanced/workshop-exercise/workshop-exercise/CustomList.cs
@@ -0,0 +1,157 @@
+﻿namespace workshop_exercise
+{
+    using System;
+
+    public class CustomList
+    {
+        private const int InitialCapacity = 2;
+
+        private int[] items;
+
+        public CustomList()
+        {
+            this.items = new int[InitialCapacity];
+        }
+
+        public int Count { get; private set; }
+
+        public int this[int index]
+        {
+            get
+            {
+                if (index >= this.Count)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+
+                return items[index];
+            }
+
+            set
+            {
+                if (index >= this.Count)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+
+                items[index] = value;
+            }
+        }
+
+        public void Resize()
+        {
+            int[] secondArray = new int[this.items.Length * 2];
+
+            for (int i = 0; i < this.items.Length; i++)
+            {
+                secondArray[i] = this.items[i];
+            }
+
+            this.items = secondArray;
+        }
+
+        public void Add(int item)
+        {
+            if (this.Count == this.items.Length)
+            {
+                this.Resize();
+            }
+
+            this.items[this.Count] = item;
+            this.Count++;
+        }
+
+        public int RemoveAt(int index)
+        {
+            if (index < 0 || index >= this.Count)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            int item = this.items[index];
+            this.items[index] = default(int);
+            this.Shift(index);
+            this.Count--;
+
+            if (this.Count <= this.items.Length / 4)
+            {
+                this.Shrink();
+            }
+
+            return item;
+        }
+
+        private void Shift(int index)
+        {
+            for (int i = index; i < this.Count - 1; i++)
+            {
+                this.items[i] = this.items[i + 1];
+            }
+        }
+
+        private void Shrink()
+        {
+            int[] secondArray = new int[this.items.Length / 2];
+
+            for (int i = 0; i < this.Count; i++)
+            {
+                secondArray[i] = this.items[i];
+            }
+
+            this.items = secondArray;
+        }
+
+        public void Insert(int index, int item)
+        {
+            if (index > this.Count)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            if (this.Count == this.items.Length)
+            {
+                this.Resize();
+            }
+
+            this.ShiftToRight(index);
+            this.items[index] = item;
+            this.Count++;
+        }
+
+        private void ShiftToRight(int index)
+        {
+            for (int i = this.Count; i > index; i--)
+            {
+                this.items[i] = this.items[i - 1];
+            }
+        }
+
+        public bool Contains(int item)
+        {
+            bool containsItem = false;
+
+            for (int i = 0; i < this.Count; i++)
+            {
+                if (this.items[i] == item)
+                {
+                    containsItem = true;
+                }
+            }
+
+            return containsItem;
+        }
+
+        public void Swap(int firstIndex, int secondIndex)
+        {
+            if (firstIndex < 0 || firstIndex >= this.Count ||
+                secondIndex < 0 || secondIndex >= this.Count)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            int tempItem = this.items[firstIndex];
+            this.items[firstIndex] = this.items[secondIndex];
+            this.items[secondIndex] = tempItem;
+        }
+    }
+}
---
+++ C# Advanced/workshop-exercise/workshop-exercise/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace workshop_exercise
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var list = new CustomList();
+            list.Add(5);
+            list.Add(6);
+            list.Add(7);
+            list.Insert(1, 8);
+            list.RemoveAt(2);
+            list.Swap(0, 2);
+            Console.WriteLine(list.Contains(9));
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine(list[i]);
+            }
+        }
+    }
+}
