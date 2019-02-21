7392991ed730fe972ed603ccd0cb0efa5fb25c02
Mert Yumer <45640355+MertYumer@users.noreply.github.com>
2019-02-21 19:27:26 +0200

    Added new exercises

---
+++ C# Advanced/10-basic-algorithms-exercises/P04-TheMatrix/TheMatrix.cs
@@ -0,0 +1,84 @@
+﻿namespace P04_TheMatrix
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class TheMatrix
+    {
+        public static int rows;
+        public static int cols;
+
+        public static void Main()
+        {
+            var coodrinates = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            rows = coodrinates[0];
+            cols = coodrinates[1];
+
+            var matrix = new char[rows][];
+
+            for (int row = 0; row < rows; row++)
+            {
+                matrix[row] = Console.ReadLine().Replace(" ", "").ToCharArray();
+            }
+
+            char symbolToFill = char.Parse(Console.ReadLine());
+            coodrinates = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int startRow = coodrinates[0];
+            int startCol = coodrinates[1];
+            char symbolToReplace = matrix[startRow][startCol];
+
+            ColorMatrix(matrix, startRow, startCol, symbolToFill, symbolToReplace);
+
+            foreach (var row in matrix)
+            {
+                Console.WriteLine(string.Join("", row));
+            }
+        }
+
+        public static void ColorMatrix(char[][] matrix, int row,
+            int col, char symbolToFill, char symbolToReplace)
+        {
+            if (!ValidCoordinates(row, col) 
+                || matrix[row][col] == symbolToFill 
+                || matrix[row][col] != symbolToReplace)
+            {
+                return;
+            }
+
+            matrix[row][col] = symbolToFill;
+            ColorMatrix(matrix, row + 1, col, symbolToFill, symbolToReplace);
+            ColorMatrix(matrix, row - 1, col, symbolToFill, symbolToReplace);
+            ColorMatrix(matrix, row, col + 1, symbolToFill, symbolToReplace);
+            ColorMatrix(matrix, row, col - 1, symbolToFill, symbolToReplace);
+        }
+
+        public static bool ValidCoordinates(int row, int col)
+        {
+            return row >= 0 && row < rows && col >= 0 && col < cols;
+        }
+    }
+
+    public class Area
+    {
+        public Area(int row, int col)
+        {
+            this.Row = row;
+            this.Col = col;
+        }
+
+        public int Row { get; set; }
+
+        public int Col { get; set; }
+
+        public int Size { get; set; }
+    }
+}
---
+++ C# Advanced/10-basic-algorithms-exercises/P05-LongestIncreasingSubsequence(LIS)/LongestIncreasingSubsequence.cs
@@ -0,0 +1,53 @@
+﻿namespace P05_LongestIncreasingSubsequence_LIS_
+{
+    using System;
+    using System.Linq;
+
+    public class LongestIncreasingSubsequence
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var length = new int[numbers.Length];
+            var previous = new int[numbers.Length];
+
+            int maxIndex = 0;
+            int maxLength = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int bestLength = 1;
+                int previousIndex = -1;
+
+                for (int j = i - 1; j >= 0; j--)
+                {
+                    if (numbers[i] > numbers[j] && length[j] + 1 >= bestLength)
+                    {
+                        bestLength = length[j] + 1;
+                        previousIndex = j;
+                    }
+                }
+
+                length[i] = bestLength;
+                previous[i] = previousIndex;
+
+                if (bestLength > maxLength)
+                {
+                    maxLength = bestLength;
+                    maxIndex = i;
+                }
+            }
+
+            var result = new int[maxLength];
+            int index = 0;
+
+            while (maxIndex != -1)
+            {
+                result[index++] = numbers[maxIndex];
+                maxIndex = previous[maxIndex];
+            }
+
+            Console.WriteLine(string.Join(" ", result.Reverse()));
+        }
+    }
+}
