5b9bc5d29a358d9a03aa2c088edbb3f1eba0f444
Mert Yumer <45640355+MertYumer@users.noreply.github.com>
2019-03-05 17:25:58 +0200

    Added new exercises

---
+++ C# OOP/03-inheritance-exercises/P01-Person/Child.cs
@@ -0,0 +1,26 @@
+﻿namespace P01_Person
+{
+    using System;
+
+    public class Child : Person
+    {
+        public Child(string name, int age) : base(name, age)
+        {
+
+        }
+
+        public override int Age
+        {
+            get => base.Age;
+            set
+            {
+                if (value > 15)
+                {
+                    throw new ArgumentException("Child's age must be less than 15!");
+                }
+
+                base.Age = value;
+            }
+        }
+    }
+}
---
+++ C# OOP/03-inheritance-exercises/P01-Person/StartUp.cs
@@ -0,0 +1,24 @@
+﻿namespace P01_Person
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            try
+            {
+                Child child = new Child(name, age);
+                Console.WriteLine(child);
+            }
+
+            catch (ArgumentException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+        }
+    }
+}
